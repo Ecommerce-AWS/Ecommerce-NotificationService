5919147642acb2cf220913c117c2c38f8e79e659
Vikas Manda <devvikasmanda@gmail.com>
2026-06-01 17:12:28 +0530

    chore: initial scaffold

---
+++ src/test/java/com/ecommerce/notificationservice/SmokeTest.java
@@ -0,0 +1,12 @@
+﻿package com.ecommerce.notificationservice;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SmokeTest {
+  @Test
+  void smoke() {
+    assertTrue(true);
+  }
+}
